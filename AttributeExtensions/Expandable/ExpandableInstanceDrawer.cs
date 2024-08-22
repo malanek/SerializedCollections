7eada150a5e555487fc91722a16383456cd18374
Mateusz <mateusz.malanowski@bugbomb.games>
2024-08-22 14:37:01 +0200

    Fix to Expandable drawer

--- AttributeExtensions/Expandable/ExpandableInstanceDrawer.cs
+++ AttributeExtensions/Expandable/ExpandableInstanceDrawer.cs
@@ -1,6 +1,7 @@
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
+
 namespace BBExtensions.AttributeDrawers
 {
     internal sealed class ExpandableInstanceDrawer
@@ -9,16 +10,19 @@ namespace BBExtensions.AttributeDrawers
         private Editor cachedEditor = null;
         private readonly SerializedProperty property;
         private readonly GUIContent label;
+        private readonly bool canDraw;
+        private Rect bgRect;
 
         public ExpandableInstanceDrawer(SerializedProperty property, GUIContent label)
         {
             this.property = property;
             this.label = label;
+            canDraw = property.propertyType != SerializedPropertyType.ObjectReference || IsPartOfArray(property);
         }
 
         public void OnGUI(Rect position)
         {
-            if (property.propertyType != SerializedPropertyType.ObjectReference)
+            if (canDraw)
             {
                 EditorGUI.LabelField(position, label.text, "Only reference values can be expandable!");
                 return;
@@ -26,23 +30,34 @@ namespace BBExtensions.AttributeDrawers
 
             EditorGUI.BeginProperty(position, label, property);
 
-            EditorGUI.PropertyField(new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight), property, label);
+            EditorGUI.PropertyField(position, property, label);
 
             if (property.objectReferenceValue != null)
             {
-                isExpanded = EditorGUI.Foldout(new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight), isExpanded, GUIContent.none, true);
+                isExpanded = EditorGUI.Foldout(position, isExpanded, GUIContent.none, true);
 
                 if (isExpanded)
                 {
                     if (cachedEditor == null)
                         Editor.CreateCachedEditor(property.objectReferenceValue, null, ref cachedEditor);
+
                     if (cachedEditor != null)
+                    {
                         cachedEditor.OnInspectorGUI();
+
+                        Rect scale = GUILayoutUtility.GetLastRect();
+                        float yOffset = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                        bgRect = new Rect(position.x, position.y + yOffset, position.width, scale.yMax - position.y - yOffset);
+
+                        EditorGUI.DrawRect(bgRect, new Color(0.1f, 0.1f, 0.1f, 0.1f));
+                    }
                 }
             }
 
             EditorGUI.EndProperty();
         }
+
+        private bool IsPartOfArray(SerializedProperty property) => property.propertyPath.Contains("Array.data");
     }
 }
 #endif

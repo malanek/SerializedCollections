ee5f418d1d74b386b28b8d54a6dbcb8d43e08020
malanek <malanek333@gmail.com>
2024-12-08 11:33:41 +0100

    IReadOnlyDictionary Extensions

---
+++ SerializedDictionary/Data/IReadOnlyDictionaryExtensions.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BBExtensions.Dictionary
+{
+    public static class IReadOnlyDictionaryExtensions
+    {
+        private static readonly Random Random = new();
+
+        public static IReadOnlyDictionary<TKey, TValue> GetRandomSubset<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, int count)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count), "Count cannot be negative.");
+
+            if (count > dictionary.Count)
+                throw new ArgumentException("Count cannot be greater than the number of elements in the dictionary.");
+
+            var keys = dictionary.Keys.OrderBy(_ => Random.Next()).Take(count);
+
+            var result = new Dictionary<TKey, TValue>();
+            foreach (var key in keys)
+            {
+                result[key] = dictionary[key];
+            }
+
+            return result;
+        }
+        
+        public static IEnumerable<TKey> GetRandomKeys<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, int count)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count), "Count cannot be negative.");
+
+            if (count > dictionary.Count)
+                throw new ArgumentException("Count cannot be greater than the number of elements in the dictionary.");
+
+            return dictionary.Keys.OrderBy(_ => Random.Next()).Take(count);
+        }
+
+        public static IEnumerable<TValue> GetRandomValues<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, int count)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count), "Count cannot be negative.");
+
+            if (count > dictionary.Count)
+                throw new ArgumentException("Count cannot be greater than the number of elements in the dictionary.");
+
+            return dictionary.Keys.OrderBy(_ => Random.Next()).Take(count).Select(key => dictionary[key]);
+        }
+    }
+}
